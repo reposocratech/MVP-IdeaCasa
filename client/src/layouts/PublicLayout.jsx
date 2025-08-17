5685a0bc8c3abe49080815ff5b31fff04602425d
Amaia <amaiavzqz@gmail.com>
2025-08-17 12:43:52 +0200

    comienzo de navbar e instalacion de nodemailer

--- client/src/layouts/PublicLayout.jsx
+++ client/src/layouts/PublicLayout.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router"
+import { PublicNavbar } from "../components/navbar/PublicNavbar"
 
 export const PublicLayout = () => {
   return (
     <>
       <header>
-
+        <PublicNavbar />
       </header>
       <main>
         <Outlet />
